5b133224a11aa875bc9044d7667c4db319392bc8
nathan-assefa <aasamnew@gmail.com>
2023-08-21 06:23:18 +0000

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -9,8 +9,8 @@ from typing import List, Tuple
 
 def index_range(page, page_size):
     """Finding indexes"""
-    start_index = (page - 1) * page_size
-    end_index = start_index + page_size
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
 
     return start_index, end_index
 
