867ef6e553aa5e742f9775ff810a0683c6eebbbe
nathan-assefa <aasamnew@gmail.com>
2023-08-21 06:26:30 +0000

    pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -46,17 +46,9 @@ class Server:
         return (pagination[range[0]:range[1]])
 
 
-def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """
-    Range of the page
-    Args:
-        page: Current page
-        page_size: Total size of the page
-    Return:
-        tuple with the range start and end size page
-    """
+def index_range(page, page_size):
+    """Finding indexes"""
+    start_index: int = (page - 1) * page_size
+    end_index: int = start_index + page_size
 
-    final_size: int = page * page_size
-    start_size: int = final_size - page_size
-
-    return (start_size, final_size)
+    return start_index, end_index
