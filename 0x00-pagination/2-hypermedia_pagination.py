7bdb145c517d3b7cd04326ec1063faabc9c8622c
nathan-assefa <aasamnew@gmail.com>
2023-08-21 07:15:53 +0000

    pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -46,3 +46,21 @@ class Server:
 
         start_index, end_index = index_range(page, page_size)
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """ returning deatiled information of pagination using dict """
+        dataset = self.dataset()
+        data = self.get_page(page, page_size)
+        data_length = len(data)
+        total_pages = math.ceil(len(dataset) / page_size)
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+                'page_size': data_length,
+                'page': page,
+                'data': data,
+                'next_page': next_page,
+                'prev_page': prev_page,
+                'total_pages': total_pages
+                }
