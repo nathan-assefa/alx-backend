0302f994226a0b151a85dc40b3d2e2bb486abbe3
nathan-assefa <aasamnew@gmail.com>
2023-08-22 18:25:10 +0000

    caching

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,17 +1,48 @@
 #!/usr/bin/env python3
-""" Basic dictionary """
+"""
+    BaseCache module
+"""
+
 from base_caching import BaseCaching
 
 
 class BasicCache(BaseCaching):
-    """Implementing basic caching algorithm"""
+    """ BasicCache define a intro to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+    """
 
     def put(self, key, item):
-        """inseting data into the cache"""
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+                item: value of the key
+        """
         if key or item is not None:
             self.cache_data[key] = item
 
     def get(self, key):
-        """Getting data from the cache"""
-        if key:
-            return self.cache_data.get(key)
+        """
+            modify cache data
+
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
