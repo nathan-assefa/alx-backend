d389d150d7d7142bad3447e38ecaea72ee6ea863
nathan-assefa <aasamnew@gmail.com>
2023-08-23 08:21:43 +0000

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -13,6 +13,7 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """Inserting data to the cache"""
+        '''
         if key or item is not None:
             key_not_exist = True if key not in self.cache_data else False
             if key_not_exist:
@@ -27,6 +28,18 @@ class LRUCache(BaseCaching):
 
             self.lru_order.insert(0, key)
 
+            self.cache_data[key] = item
+        '''
+        if key and item:
+            length = len(self.cache_data)
+            if key not in self.cache_data:
+                if length >= BaseCaching.MAX_ITEMS:
+                    lru_key = self.lru_order.pop()
+                    del self.cache_data[lru_key]
+                    print("DISCARD:", lru_key)
+            else:
+                self.lru_order.remove(key)
+            self.lru_order.insert(0, key)
             self.cache_data[key] = item
 
 
