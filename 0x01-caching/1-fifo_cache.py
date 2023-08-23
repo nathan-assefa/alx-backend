45cd830a18d93a3da8afce1d024de998d6b6de08
nathan-assefa <aasamnew@gmail.com>
2023-08-23 07:42:27 +0000

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -13,18 +13,15 @@ class FIFOCache(BaseCaching):
     def put(self, key, item):
         """Inserting data to the cache"""
         if not key and not item:
-            return
+            pass
 
         # Check if the key is not precent in the cache_data
         key_not_exist = True if key not in self.cache_data else False
         
-        if key_not_exist:
-            if len(self.cache_data) >= self.MAX_ITEMS and key_not_exist:
-                discard_key = next(iter(self.cache_data))
-                del self.cache_data[discard_key]
-                print("DISCARD: {}".format(discard_key))
-        else:
-            del self.cache_data[key]
+        if len(self.cache_data) >= self.MAX_ITEMS and key_not_exist:
+            discard_key = next(iter(self.cache_data))
+            del self.cache_data[discard_key]
+            print("DISCARD: {}".format(discard_key))
 
         self.cache_data[key] = item
 
