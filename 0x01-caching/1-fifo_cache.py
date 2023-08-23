fc206b1833c645dc58c01d3f12c1fcba99b50254
nathan-assefa <aasamnew@gmail.com>
2023-08-23 18:41:21 +0000

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -9,7 +9,6 @@ class FIFOCache(BaseCaching):
     def __init__(self):
         """intializing attributes"""
         super().__init__()
-        self.order = []
 
     def put(self, key, item):
         """Inserting data to the cache"""
@@ -18,10 +17,9 @@ class FIFOCache(BaseCaching):
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
-                print("DISCARD: {}".format(self.order[0]))
-                del self.cache_data[self.order[0]]
-                del self.order[0]
-            self.order.append(key)
+                fifo_key = next(iter(self.cache_data.keys()))
+                del self.cache_data[fifo_key]
+                print("DISCARD: {}".format(fifo_key))
             self.cache_data[key] = item
 
         """
