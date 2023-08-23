416564a65b5b06c11db9582f8cdb93f71d35fa56
nathan-assefa <aasamnew@gmail.com>
2023-08-23 07:40:24 +0000

    caching

--- 0x01-caching/2-lifo_cache.py
+++ 0x01-caching/2-lifo_cache.py
@@ -16,7 +16,7 @@ class LIFOCache(BaseCaching):
         Cache a key-value pair
         """
         if key is None or item is None:
-            pass
+            return
         else:
             length = len(self.cache_data)
             if length >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
@@ -27,6 +27,27 @@ class LIFOCache(BaseCaching):
                 del self.order[self.order.index(key)]
             self.order.append(key)
             self.cache_data[key] = item
+
+        '''
+         key_not_exist = True if key not in self.cache_data else False
+
+        if key_not_exist:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discard_key = next(iter(reversed(self.cache_data)))
+                # or we can use this
+                # last_key = list(self.cache_data.keys())[-1]
+
+                del self.cache_data[discard_key]
+
+                print("DISCARD: {}".format(discard_key))
+
+        else:
+            # If the value is not None, indicating an existing key, it deletes
+            # +the existing key-value pair from the cache before updating it.
+            del self.cache_data[key]
+
+        self.cache_data[key] = item
+        '''
 
     def get(self, key):
         """Getting data from the cache"""
