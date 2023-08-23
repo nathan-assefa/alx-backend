681ae86381ce66a666cb55a9eced4193da6a79fb
nathan-assefa <aasamnew@gmail.com>
2023-08-23 09:56:44 +0000

    caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -13,39 +13,41 @@ class LFUCache(BaseCaching):
         self.lfu_order = {}
 
     def put(self, key, item):
+        ''' puting item to the cache '''
         if key is not None and item is not None:
-            self.cache_data[key] = item  # Add the key-value pair to the cache
+            key_not_exist = key not in self.cache_data
+            if key_not_exist:
+                # Update the frequency of the key and its usage order
+                if key in self.frequency:
+                    self.frequency[key] += 1
+                else:
+                    self.frequency[key] = 1
+                freq = self.frequency[key]
 
-            # Update the frequency of the key and its usage order
-            if key in self.frequency:
-                self.frequency[key] += 1
-            else:
-                self.frequency[key] = 1
-            freq = self.frequency[key]
+                if freq in self.lfu_order:
+                    self.lfu_order[freq].append(key)
+                else:
+                    self.lfu_order[freq] = [key]
 
-            if freq in self.lfu_order:
-                self.lfu_order[freq].append(key)
-            else:
-                self.lfu_order[freq] = [key]
+                self.cache_data[key] = item
 
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                # Find the least frequency used item
-                min_freq = min(self.lfu_order)
-                lfu_key = self.lfu_order[min_freq].pop(
-                    0
-                )  # Remove the least frequency used key
+                if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+                    min_freq = min(self.lfu_order)
+                    lfu_key = self.lfu_order[min_freq].pop(0)
 
-                if not self.lfu_order[min_freq]:
-                    del self.lfu_order[min_freq]
+                    if not self.lfu_order[min_freq]:
+                        del self.lfu_order[min_freq]
 
-                del self.cache_data[
-                    lfu_key
-                ]  # Delete the corresponding key from the cache
-                del self.frequency[lfu_key]  # Delete the frequency record
-                print("DISCARD:", lfu_key)
+                    del self.cache_data[lfu_key]
+                    del self.frequency[lfu_key]
+                    print("DISCARD:", lfu_key)
+        else:
+            del self.cache_data[key]
+            self.cache_data[key] = item 
 
     def get(self, key):
-        if key in self.cache_data:
+        ''' getting data from the catch '''
+        if key and key in self.cache_data:
             # Update the frequency and usage order for the accessed key
             freq = self.frequency[key]
             self.frequency[key] = freq + 1
@@ -56,6 +58,6 @@ class LFUCache(BaseCaching):
             else:
                 self.lfu_order[freq + 1] = [key]
 
-            return self.cache_data[key]  # Return the value associated with the key
+            return self.cache_data[key]
         else:
             return None
