785827051adbb63c394645670c8ac761ff7b44de
nathan-assefa <aasamnew@gmail.com>
2023-08-23 19:20:42 +0000

    caching

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -4,63 +4,65 @@ from base_caching import BaseCaching
 
 
 class LFUCache(BaseCaching):
-    """LFUCache defines an LFU caching system"""
+    """
+    LFUCache defines an LFU caching system
+    """
 
     def __init__(self):
-        """Initialize the LFUCache"""
+        """
+        Initialize the class with the parent's init method
+        """
         super().__init__()
-        # To track the frequency of each key
-        self.frequency = {}
-        # To maintain the order of usage for each frequency
-        self.lfu_order = {}
+        self.usage_order = []  # To maintain the order of usage
+        self.key_frequency = {}  # To track the frequency of each key
 
     def put(self, key, item):
-        """Cache a key-value pair"""
-        if key is not None and item is not None:
-            key_not_exist = True if key not in self.cache_data else False
-            if len(self.cache_data) >= BaseCaching.MAX_ITEMS and key_not_exist:
-                # Find the least frequency used item
-                min_freq = min(self.lfu_order)
-                # Remove the least frequency used key
-                lfu_key = self.lfu_order[min_freq].pop(0)
+        """
+        Cache a key-value pair
+        """
+        if key is None or item is None:
+            return
 
-                if not self.lfu_order[min_freq]:
-                    del self.lfu_order[min_freq]
+        cache_size = len(self.cache_data)
+        if cache_size >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+            min_frequency = min(self.key_frequency.values())
+            least_frequent_keys = [
+                k for k, v in self.key_frequency.items() if v == min_frequency
+            ]
 
-                # Delete the corresponding key from the cache
-                del self.cache_data[lfu_key]
-                # Delete the frequency record
-                del self.frequency[lfu_key]
-                print("DISCARD:", lfu_key)
-
-            # Update the frequency of the key and its usage order
-            if key in self.frequency:
-                self.frequency[key] += 1
+            if len(least_frequent_keys) > 1:
+                lru_least_frequent = {
+                    k: self.usage_order.index(k) for k in least_frequent_keys
+                }
+                discard_key = min(
+                        lru_least_frequent, key=lru_least_frequent.get
+                        )
             else:
-                self.frequency[key] = 1
-            freq = self.frequency[key]
+                discard_key = least_frequent_keys[0]
 
-            if freq in self.lfu_order:
-                self.lfu_order[freq].append(key)
-            else:
-                self.lfu_order[freq] = [key]
+            print("DISCARD:", discard_key)
+            del self.cache_data[discard_key]
+            self.usage_order.remove(discard_key)
+            del self.key_frequency[discard_key]
 
-            # Add the key-value pair to the cache
-            self.cache_data[key] = item
+        if key in self.key_frequency:
+            self.key_frequency[key] += 1
+        else:
+            self.key_frequency[key] = 1
+
+        if key in self.usage_order:
+            self.usage_order.remove(key)
+
+        self.usage_order.append(key)
+        self.cache_data[key] = item
 
     def get(self, key):
-        """Return the value linked to a given key, or None"""
-        if key in self.cache_data:
-            # Update the frequency and usage order for the accessed key
-            freq = self.frequency[key]
-            self.frequency[key] = freq + 1
-            self.lfu_order[freq].remove(key)
-
-            if freq + 1 in self.lfu_order:
-                self.lfu_order[freq + 1].append(key)
-            else:
-                self.lfu_order[freq + 1] = [key]
-
+        """
+        Return the value linked to a given key, or None
+        """
+        if key is not None and key in self.cache_data:
+            self.usage_order.remove(key)
+            self.usage_order.append(key)
+            self.key_frequency[key] += 1
             return self.cache_data[key]
-        else:
-            return None
+        return None
