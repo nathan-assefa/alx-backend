c4dd29ad8bc284834c8083b77e0e04cb89e1a9c1
nathan-assefa <aasamnew@gmail.com>
2023-08-23 06:38:11 +0000

    caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -1,13 +1,42 @@
-#!/usr/bin/env python3
-""" FIFO caching """
+#!/usr/bin/python3
+"""
+    BaseCache module
+"""
+
 from base_caching import BaseCaching
 
 
 class FIFOCache(BaseCaching):
-    """implementing FIFO caching algorithm"""
+    """ FIFOCache define a FIFO algorithm to use cache
+
+      To use:
+      >>> my_cache = BasicCache()
+      >>> my_cache.print_cache()
+      Current cache:
+
+      >>> my_cache.put("A", "Hello")
+      >>> my_cache.print_cache()
+      A: Hello
+
+      >>> print(my_cache.get("A"))
+      Hello
+
+      Ex:
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Holberton", D: "School"}
+      >>> my_cache.put("C", "Street")
+      >>> print(self.cache_data)
+      {A: "Hello", B: "World", C: "Street", D: "School"}
+
+      >>> my_cache.put("F", "COD")
+      DISCARD: A
+      >>> print(self.cache_data)
+      {F: "COD", B: "World", C: "Holberton", D: "School"}
+    """
 
     def __init__(self):
-        """intializing attributes"""
+        """ Initiliaze
+        """
         super().__init__()
 
     def put(self, key, item):
@@ -28,10 +57,16 @@ class FIFOCache(BaseCaching):
 
             self.cache_data[key] = item
 
-
     def get(self, key):
-        """Getting data from the cache"""
-        if not key or key not in self.cache_data:
-            return None
+        """
+            modify cache data
 
-        return self.cache_data[key]
+            Args:
+                key: of the dict
+
+            Return:
+                value of the key
+        """
+
+        valuecache = self.cache_data.get(key)
+        return valuecache
