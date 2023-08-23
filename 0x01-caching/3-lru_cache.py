f8197e27a3b0f26bf2b3c9c106f7c31c489957f5
nathan-assefa <aasamnew@gmail.com>
2023-08-23 07:51:50 +0000

    caching

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,90 +1,42 @@
-#!/usr/bin/python3
-"""
-    BaseCache module
-"""
-
+#!/usr/bin/env python3
+""" LRU caching """
 from base_caching import BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """ LRUCache define a LRU algorithm to use cache
-
-      To use:
-      >>> my_cache = BasicCache()
-      >>> my_cache.print_cache()
-      Current cache:
-
-      >>> my_cache.put("A", "Hello")
-      >>> my_cache.print_cache()
-      A: Hello
-
-      Ex:
-      >>> my_cache.print_cache()
-      Current cache:
-      A: Hello
-      B: World
-      C: Holberton
-      D: School
-      >>> print(my_cache.get("B"))
-      World
-      >>> my_cache.put("E", "Battery")
-      DISCARD: A
-      >>> my_cache.print_cache()
-      Current cache:
-      B: World
-      C: Holberton
-      D: School
-      E: Battery
-    """
+    """implementing LRU caching algorithm"""
 
     def __init__(self):
-        """ Initiliaze
-        """
+        """intializing attributes"""
         super().__init__()
-        self.leastrecent = []
+        self.lru_order = []
 
     def put(self, key, item):
-        """
-            modify cache data
-
-            Args:
-                key: of the dict
-                item: value of the key
-        """
+        """Inserting data to the cache"""
         if key or item is not None:
-            valuecache = self.get(key)
-            # Make a new
-            if valuecache is None:
+            key_not_exist = True if key not in self.cache_data else False
+            if key_not_exist:
                 if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
-                    keydel = self.leastrecent
-                    lendel = len(keydel) - 1
-                    del self.cache_data[keydel[lendel]]
-                    print("DISCARD: {}".format(self.leastrecent.pop()))
+                    # Discard the least recently used item
+                    lru_key = self.lru_order.pop()
+                    del self.cache_data[lru_key]
+                    print("DISCARD:", lru_key)
+
             else:
                 del self.cache_data[key]
 
-            if key in self.leastrecent:
-                self.leastrecent.remove(key)
-                self.leastrecent.insert(0, key)
-            else:
-                self.leastrecent.insert(0, key)
+            if key in self.lru_order:
+                self.lru_order.remove(key)
+            self.lru_order.insert(0, key)
 
             self.cache_data[key] = item
 
     def get(self, key):
-        """
-            modify cache data
-
-            Args:
-                key: of the dict
-
-            Return:
-                value of the key
-        """
-        valuecache = self.cache_data.get(key)
-
-        if valuecache:
-            self.leastrecent.remove(key)
-            self.leastrecent.insert(0, key)
-
-        return valuecache
+        """Getting data from the cache"""
+        if key and key in self.cache_data:
+            # Move the accessed key to the front of the LRU order
+            self.lru_order.remove(key)
+            self.lru_order.insert(0, key)
+            return self.cache_data[key]
+        else:
+            return None
