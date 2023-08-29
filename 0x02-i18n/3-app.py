ece685c4d86f97b24d0caf258bc8bfeffa79b649
nathan-assefa <aasamnew@gmail.com>
2023-08-29 10:05:28 +0000

    i18n

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -1,42 +1,40 @@
 #!/usr/bin/env python3
-from flask_babel import Babel, request
-from flask import Flask, render_template
+""" Basic babel setup"""
 
 
-class Config:
-    """Defining the configuration attributes"""
-
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
-
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from os import getenv
 
 app = Flask(__name__)
+babel = Babel(app)
 
-# Using 'from_object' loading configuration settings
-# + from the specified object, in this case from Config class
+
+class Config(object):
+    """ configure babel """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
 app.config.from_object(Config)
 
-# Instantiate the Babel object
-babel = Babel(app)
+
+@app.route('/', methods=['GET'], strict_slashes=False)
+def index() -> str:
+    """
+    Return: 1-index.html
+    """
+    return render_template('3-index.html')
 
 
 @babel.localeselector
 def get_locale() -> str:
-    """
-    Determining the best match with our supported languages.
-    **** what is localselector? *****
-    Flask-Babel relies on the locale_selector to determine which
-    language to use for rendering content in your Flask application.
-    """
-    return request.accept_languages.best_match(app.config["LANGUAGES"])
-
-
-@app.route("/", strict_slashes=False)
-def hello():
-    """Rendering hello world"""
-    return render_template("3-index.html")
+    """ Determines best match based on supported languages """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    host = getenv("API_HOST", "0.0.0.0")
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port)
