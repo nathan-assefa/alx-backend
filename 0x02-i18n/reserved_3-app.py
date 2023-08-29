72d0989024a4fc2853537436bf37bc7fa58ebc79
nathan-assefa <aasamnew@gmail.com>
2023-08-29 13:44:28 +0000

    i18n

--- 0x02-i18n/reserved_3-app.py
+++ 0x02-i18n/reserved_3-app.py
@@ -21,6 +21,9 @@ babel = Babel(app)
 def get_locale() -> str:
     """
     Determining the best match with our supported languages.
+    **** what is localselector? *****
+    Flask-Babel relies on the locale_selector to determine which
+    language to use for rendering content in your Flask application.
     """
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
