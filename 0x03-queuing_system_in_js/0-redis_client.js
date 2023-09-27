2a1d266f7bfc467886635aa96b5f1a93ff9ff679
nathan-assefa <aasamnew@gmail.com>
2023-09-27 06:01:04 +0000

    redis client

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,15 @@
+// Import the node_redis library using ES6 import syntax
+import redis from 'redis';
+
+// Create a Redis client instance
+const client = redis.createClient();
+
+// Handle the "ready" event when the client successfully connects to Redis
+client.on('ready', () => {
+  console.log('Redis client connected to the server');
+});
+
+// Handle any errors that occur during the connection
+client.on('error', (error) => {
+  console.error(`Redis client not connected to the server: ${error}`);
+});
